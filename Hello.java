3f0e46f729eabb8f99e37581e604aba588f3fbe5
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-07-21 11:53:37 +0530

    adding Hello program

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello
+{
+        public static void main(String args[])
+        {
+                while(true)
+                {
+                        System.out.println("Hello World");
+                        try
+                        {
+                                Thread.sleep(10000);
+                        }
+                        catch(Exception e)
+                        {
+                                System.out.println(e);
+                        }
+                }
+        }
+}
